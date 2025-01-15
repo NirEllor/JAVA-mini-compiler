e45d7fdd5ed1a508285b7033080a607ed7487ba7
Nir Ellor <nirellorwaizner@gmail.com>
2025-01-15 17:36:07 +0200

    Symbol tables are ready

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -58,7 +58,8 @@ public class SymbolTable {
 
     // Helper function to find the scope where a variable is declared
     private int findVariableScope(String name) {
-        for (int scope = currentScope; scope >= 0; scope--) {
+        for (int scope = currentScope; scope >= 1; scope--) {
+            System.out.println(scope);
             if (variablesMap.containsKey(scope) && variablesMap.get(scope).containsKey(name)) {
                 return scope;
             }
@@ -84,41 +85,17 @@ public class SymbolTable {
 
         try {
             st.enterScope();
-            st.declareVariable("x", "int", 5);
-            st.declareVariable("y", "String", "Hello");
-            st.declareVariable("z", "double", 3.0);
-            st.declareVariable("c", "char", '!');
-            st.declareVariable("d", "char", "!");
-            st.declareVariable("flag", "boolean", "true");
-
+            st.declareVariable("x", "int", 1);
+//            System.out.println(st.findVariableScope("x"));
             st.enterScope();
-            st.assignValue("x", 20);
-            st.assignValue("y", 30);
-            st.assignValue("z", 0.0005);
-            st.declareVariable("d","char", '!');
-            st.assignValue("flag", false);
-            System.out.println(st.getType("x"));
-            st.getType("y");
-            st.getType("z");
-            st.getType("c");
-            st.getType("d");
-            st.getValue("x");
-            st.getValue("y");
-            st.getValue("z");
-            st.getValue("c");
-            st.getValue("d");
-            System.out.println(st.getValue("flag"));
+            st.declareVariable("x", "int", 1);
+//            System.out.println(st.findVariableScope("x"));
+//            st.exitScope();
+//            System.out.println(st.findVariableScope("x"));
+//            st.declareVariable("x", "float", 2);
+            st.assignValue("y", 2);
 
-            st.enterScope();
-            st.declareVariable("x", "int", 5);
-            st.assignValue("x", 6);
-            st.printSymbolTable();
 
-            st.exitScope();
-            st.printSymbolTable();
-
-            st.exitScope();
-            st.printSymbolTable();
         }
         catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
