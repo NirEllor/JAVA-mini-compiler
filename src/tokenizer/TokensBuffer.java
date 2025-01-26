3ce833236bccde4fcf0b64c715172597c3c3e225
Nir Ellor <nirellorwaizner@gmail.com>
2025-01-26 17:59:04 +0200

    pre-submit debug

--- src/tokenizer/TokensBuffer.java
+++ src/tokenizer/TokensBuffer.java
@@ -11,20 +11,39 @@ public class TokensBuffer {
     private final String currentToken;
     private final List<String> tokens;
 
+    /**
+     * Constructs a new {@code TokensBuffer} with the specified index, current token, and token list.
+     *
+     * @param index        the current index in the token list
+     * @param currentToken the token currently being processed
+     * @param tokens       the full list of tokens
+     */
     public TokensBuffer(int index, String currentToken, List<String> tokens) {
         this.index = index;
         this.currentToken = currentToken;
         this.tokens = tokens;
     }
-
+    /**
+     * Returns the current token being processed.
+     *
+     * @return the current token
+     */
     public String getCurrentToken() {
         return currentToken;
     }
-
+    /**
+     * Returns the current index in the token list.
+     *
+     * @return the current index
+     */
     public int getIndex() {
         return index;
     }
-
+    /**
+     * Returns the full list of tokens.
+     *
+     * @return the list of tokens
+     */
     public List<String> getTokens() {
         return tokens;
     }
